88b6d009efa7a601f4bb2b83ffda1b193cd740c3
Max-Meldrum <max@meldrum.se>
2024-07-12 09:15:00 +0000

    Deploying to gh-pages from @ uwheel/uwheel.github.io@8588bd88e45d9726171edbc9748d72489493cbc1 🚀

--- src/data/tags.ts
+++
@@ -1,6 +0,0 @@
-import { getPublishedPosts } from "./posts";
-
-export async function getUniqueTags(): Promise<string[]> {
-  const posts = await getPublishedPosts();
-  return [...new Set(posts.flatMap((post) => post.data.tags))].sort();
-}
--- uno.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "unocss";
-import unocss from "unocss/preset-uno";
-import typography from "unocss/preset-typography";
-
-export default defineConfig({
-  presets: [unocss(), typography()],
-});
